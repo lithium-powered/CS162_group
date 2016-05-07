38ae00280eb5f128354db0427459ed20dc839311
Annie Lin <annie_lin@berkeley.edu>
2016-05-07 02:45:38 +0000

    everything

---
+++ pintos/src/filesys/filesys.h
@@ -0,0 +1,23 @@
+#ifndef FILESYS_FILESYS_H
+#define FILESYS_FILESYS_H
+
+#include <stdbool.h>
+#include "filesys/off_t.h"
+#include "threads/thread.h"
+#include "threads/malloc.h"
+
+/* Sectors of system file inodes. */
+#define FREE_MAP_SECTOR 0       /* Free map file inode sector. */
+#define ROOT_DIR_SECTOR 1       /* Root directory file inode sector. */
+
+/* Block device that contains the file system. */
+struct block *fs_device;
+struct lock globalCacheLock;
+
+void filesys_init (bool format);
+void filesys_done (void);
+bool filesys_create (const char *name, off_t initial_size);
+struct file *filesys_open (const char *name);
+bool filesys_remove (const char *name);
+
+#endif /* filesys/filesys.h */
